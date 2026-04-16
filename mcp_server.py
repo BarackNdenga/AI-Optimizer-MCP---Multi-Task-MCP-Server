260d7add41f4804c021e0fee38f154f20b92cbb2
BarackNdenga <ndengabarack@gmail.com>
2026-04-16 01:18:05 +0100

    mcp_sever
    ok

---
+++ mcp_server.py
@@ -0,0 +1,5 @@
+# DEPRECATED: Use 'python -m ai_optimizer_mcp.server'
+# Full code refactored to src/ai_optimizer_mcp/
+print("Migrate to new package: pip install -e . && python -m ai_optimizer_mcp.server")
+
+
